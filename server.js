f7645e94eb32807114c0fdaf6c20329064b750a8
Tim <tmetcalfe89@gmail.com>
2022-03-24 09:50:55 -0700

    Added landing page.

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const routes = require("./routes");
 
@@ -7,6 +8,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(routes);
 
 app.listen(PORT, () => {
